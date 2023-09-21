8922b58934d9664a474c795eb1c5bf508982d1ef
madhavkumarthakur <108406245+madhavkumarthakur@users.noreply.github.com>
2023-09-20 19:02:23 -0700

    Add files via upload

---
+++ organize.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+
+from_dir="C:\Users\Madhav Kumar Thakur\Downloads"
+to_dir="C:\Users\Madhav Kumar Thakur\Documents"
+
+list_of_files=os.listdir(from_dir)
+
+print(list_of_files)
+for file_name in list_of_files:
+    name,extension=os.path.splitext(file_name)
+
+    if extension=="":
+        continue
+    if extension in [".txt",".doc",".docx",".pdf"]:
+        path1=from_dir+"/"+file_name
+        path2=to_dir+"/"+"Document_Files"
+        path3=to_dir+"/"+"Document_Files"+"/"+file_name
+
+    if os.path.exists(path2):
+        shutil.move(path1,path3)
+    else:
+        os.makedirs(path2)
+        shutil.move(path1,path3)
